9e21780d8260d3cbad60a8b28a84d8d969ed39e3
johnkirch123 <john.kirch.business@gmail.com>
2017-12-29 20:46:46 -0700

    Initial commit

---
+++ chris-hawkes/python-for-beginners/firstFile.py
@@ -0,0 +1,5 @@
+my_variable = "<html><head><title>Look at this!</title><body><h1>Johnny's work!</h1><a href='http://www.hipstercode.com'>Click me!</a></body></head></html>"
+
+my_html_file = open("/Users/Johnny/Desktop/programs-and-testing/python/chris-hawkes/python-for-beginners/my_html_file.html", "w")
+
+my_html_file.write(my_variable)
