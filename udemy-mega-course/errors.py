cbccb81f4b6d0d2f8c7a0e66ede21e6a93128f74
John Kirch <john.kirch.business@gmail.com>
2017-12-31 13:46:50 -0700

    error testing
    Testing various errors and how to resolve.

--- udemy-mega-course/errors.py
+++ udemy-mega-course/errors.py
@@ -11,3 +11,20 @@ a = 1
 b = "2"
 print(int(2.5))
 print(a + int(b))
+c = 3
+#print(c/0)
+def divide(a, b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return "You are dividing by 0"
+    except TypeError:
+        return "Incompatible types"
+    except NameError:
+        return "Input is not a number"
+
+print(divide(1, 0))
+print(divide(1, 1))
+print(divide(1, "hat"))
+#print(divide(1, z)) still showing up as an error, not catching exception???
+print("End of Program")
